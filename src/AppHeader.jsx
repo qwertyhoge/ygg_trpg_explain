53ef9f69e3f94a8ad4f66df64334e3e552094ccc
qwertyhoge <cood.gh47@hotmail.co.jp>
2023-04-27 12:55:07 +0900

    added top padding of header

--- src/AppHeader.jsx
+++ src/AppHeader.jsx
@@ -2,11 +2,12 @@ function AppHeader(props){
     const headerStyles = {
         background: '#f6f8ff',
         minHeight: '90px',
-        position: 'relative'
+        position: 'relative',
+        paddingTop: '10px'
     };
     const breadcrumbStyles = {
         position: 'absolute',
-        bottom: '3px'
+        bottom: '5px'
     };
 
     const pageTitleStyles = {
@@ -14,7 +15,8 @@ function AppHeader(props){
         fontSize: '2.6em',
         letterSpacing: '0.2em',
         color: '#3b3c98e8',
-        textShadow: '1px 1px #ccd2e4'
+        textShadow: '1px 1px #ccd2e4',
+        margin: '0px'
     }
     
     return (
