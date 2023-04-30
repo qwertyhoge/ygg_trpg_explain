da830ab70387fa502a69b33a1c7fd532e8244017
qwertyhoge <cood.gh47@hotmail.co.jp>
2023-04-30 15:01:23 +0900

    added page of Battle

---
+++ src/Battle.jsx
@@ -0,0 +1,12 @@
+import PageBase from "./PageBase";
+import ProgressCard from "./ProgressCard";
+
+function Battle(props){
+    return <PageBase header={props.header}>
+        <ProgressCard title="">
+
+        </ProgressCard>
+    </PageBase>
+}
+
+export default Battle;
