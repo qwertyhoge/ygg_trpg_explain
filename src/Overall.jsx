70f4da977a72f7391ab59d8c7c7b39c402107f3f
qwertyhoge <cood.gh47@hotmail.co.jp>
2023-04-30 21:06:04 +0900

    added notification

--- src/Overall.jsx
+++ src/Overall.jsx
@@ -3,6 +3,10 @@ import ProgressCardLinks from "./ProgressCardLinks";
 import PageBase from "./PageBase";
 
 function Overall(props){
+    const notificationLinks = <ProgressCardLinks
+        pageSetter={props.pageSetter}
+        detail="houserules"
+    ></ProgressCardLinks>
     const characterMakingLinks = <ProgressCardLinks
         pageSetter={props.pageSetter}
         reference="external:http://ygg.sakura.ne.jp/yg002.html"
@@ -16,6 +20,28 @@ function Overall(props){
 
     return (
         <PageBase header={props.header}>
+            <ProgressCard title="諸注意" links={notificationLinks}>
+                セッション上留意してほしいこと色々。またハウスルールは右のリンクから。ただし用語の説明はしていないので、一通りルールを読んでから戻ってくるのを推奨。
+                <ul>
+                    <li>
+                        システムがうるさいくらいにバランスブレイカーな設定を嫌っているので、良識の範囲内で作ってほしい。
+                    </li>
+                    <li>
+                        RP推奨。シーン中のキャラの探索や行動回数制限付きの行動など。音声/テキストは問わない。
+                        <ul>
+                            <li>
+                                予定ではGMは音声とテキストの両方をやります(RPとして+ログ残し)。
+                            </li>
+                            <li>
+                                キャラクターの設定はしっかり練っておくとよいと思う。
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        他PLの迷惑になるプレイングは控えてほしい(正直心配はしてません)。
+                    </li>
+                </ul>
+            </ProgressCard>
             <ProgressCard title="キャラクター作成" links={characterMakingLinks}>
                 プロフィールだけでなくライフパスといったキャラクターの背景を描くための要素が豊富にあり、ロールプレイを好むユグドラTRPGの性格が如実に表れている。そしてこのシステムの真骨頂が能力作成。コンセプトを決め、詳細をじっくりと練って作りたい。
             </ProgressCard>
